e8d4bdaa8897d7926dfb31f3366653904d641c96
Håkan Thörngren <hth313@gmail.com>
2016-05-30 22:45:45 -0700

    Update to use Elm 0.17
    Get a calculator to show with static text in the LCD using suitable
    fonts and positions.

--- src/HP41.elm
+++ src/HP41.elm
@@ -1,36 +1,103 @@
-import Color exposing (black)
-import Graphics.Element exposing (..)
-import Text exposing (defaultStyle)
+module HP41 exposing (main)
 
-main = calc
+{-| This is a user interface for showing an HP-41 calculator and interacting
+with dbnut.
 
+# Definition
+@docs main
+
+-}
+
+
+import Html exposing (Html)
+import Html.App as Html
+import Keyboard
+import Svg exposing (..)
+import Svg.Attributes  exposing (..)
+import Task
+import Time
+import WebSocket
+
+
+{-| The main program entry point -}
+main : Program Never
+main = Html.program { init = init, update = update, view = view,
+                      subscriptions = subscriptions }
+
+-- | The size of the calculator image
 calcWidth  = 298
 calcHeight = 557
 
-background = image calcWidth calcHeight "image/hp41.png"
 
-calc = flow outward [background, lcdContainer, ann]
+-- | How much we offset in LCD characters and annunciators
+offsetLCD = "30"
 
-lcdContainer = container calcWidth 86 middle
-               <| width calcWidth
-               <| height lcdHeight
-               <| centered
-               <| Text.style lcdStyle
-               <| Text.fromString "HELLO WORLD2"
+-- background = image calcWidth calcHeight "image/hp41.png"
 
-ann = container calcWidth 65 midBottom
-      <| width calcWidth
-      <| height annHeight
-      <| centered
-      <| Text.style annStyle
-      <| Text.fromString "BAT USER GRAD SHIFT O1234 PRGM ALPHA"
+calc model =
+  Svg.svg [ width (toString calcWidth)
+          , height (toString calcHeight)]
+     [ image [ width (toString calcWidth)
+             , height (toString calcHeight)
+             , xlinkHref "image/hp41.png"
+             ]
+             []
+     , text' [ fontSize "23"
+             , Svg.Attributes.style "font-family: 'HP41'"
+             , x offsetLCD
+             , y "48" ]
+         [ text "hello world!"]
+     , text' [ fontSize "11"
+             , x offsetLCD
+             , y "62"
+             , Svg.Attributes.style "font-family: 'Andale Mono'"]
+         [ text "BAT USER GRAD SHIFT O1234 PRGM ALPHA" ]
+     ]
 
-lcdStyle : Text.Style
-lcdStyle = { defaultStyle | typeface = ["HP41"], height = Just lcdHeight }
 
-lcdHeight = 23
+type alias Model = {
+    lcd : String
+  , annunciators : String
+}
 
-annStyle : Text.Style
-annStyle = { defaultStyle | typeface = ["Andale Mono"], height = Just annHeight }
 
-annHeight = 11
+init : (Model, Cmd Msg)
+init = (blankLCD, Cmd.none)
+
+
+blankLCD : Model
+blankLCD = { lcd = "            "
+           , annunciators = "                                    " }
+
+
+type Msg = UpdateLCD String String
+         | KeyEvent Bool Int
+         | SendMessage String
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    UpdateLCD text ann -> ({ model | lcd = text, annunciators = ann }, Cmd.none)
+    KeyEvent pressed kc -> (model, Task.perform (\_ -> SendMessage "")
+                                                (sendKey pressed kc) Time.now)
+    SendMessage body -> (model, WebSocket.send webSocket body)
+
+
+view : Model -> Html Msg
+view model = calc model
+
+
+subscriptions model =
+  Sub.batch [ Keyboard.downs (KeyEvent True)
+            , Keyboard.ups (KeyEvent False)
+            , WebSocket.keepAlive webSocket]
+
+-- 112 is F1
+-- keyEvent pressed kc = KeyEvent (KeyData pressed timestamp kc)
+
+sendKey pressed kc time =
+  SendMessage (toString (round (Time.inMilliseconds time)))
+
+
+webSocket = "ws://localhost:8080"
