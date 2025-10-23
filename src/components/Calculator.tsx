71ca38dedc53879414b92eed64e1bf259ad74ecb
Håkan Thörngren <hth313@gmail.com>
2025-10-23 14:21:41 -0700

    Obey leading whitespace

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -191,6 +191,7 @@ export function Calculator({ onKeyEvent, lcdText, annunciators }: CalculatorProp
     lcdTextElement.setAttribute('x', '30');
     lcdTextElement.setAttribute('y', '48');
     lcdTextElement.setAttribute('xml:space', 'preserve');
+    lcdTextElement.setAttribute('style', 'white-space: pre');
     lcdTextElement.textContent = lcdText || "MEMORY LOST";
     svg.appendChild(lcdTextElement);
 
@@ -201,6 +202,7 @@ export function Calculator({ onKeyEvent, lcdText, annunciators }: CalculatorProp
     annElement.setAttribute('x', '30');
     annElement.setAttribute('y', '62');
     annElement.setAttribute('xml:space', 'preserve');
+    annElement.setAttribute('style', 'white-space: pre');
     annElement.setAttribute('font-family', 'Andale Mono');
     annElement.textContent = annunciators || "";
     svg.appendChild(annElement);
